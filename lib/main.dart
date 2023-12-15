7d9f1099bb6ca524d598631e25917795603ee784
Miraj Hossain <miraj09hossain@gmail.com>
2023-12-15 21:27:40 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
           title: 'Flutter Demo',
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: false,
+            useMaterial3: true,
           ),
           home: const SignInPage(),
           routes: {
